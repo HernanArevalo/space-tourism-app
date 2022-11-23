ca7e3cae42009abad9ac2b63d75a184f5351d6a3
Hernan Arevalo <hernanarevalo16@gmail.com>
2022-11-23 18:23:19 -0300

    ./ in routes

--- src/router/SpaceRouter.jsx
+++ src/router/SpaceRouter.jsx
@@ -4,10 +4,10 @@ import { CrewPage, DestinationPage, HomePage, TechnologyPage } from '../pages'
 export const SpaceRouter = () => {
   return (
     <Routes>
-        <Route path="/home" element={ <HomePage />}/>
-        <Route path="/destination/:destination" element={ <DestinationPage />}/>
-        <Route path="/crew/:member" element={ <CrewPage />}/>
-        <Route path="/technology/:tech" element={ <TechnologyPage />}/>
+        <Route path="./home" element={ <HomePage />}/>
+        <Route path="./destination/:destination" element={ <DestinationPage />}/>
+        <Route path="./crew/:member" element={ <CrewPage />}/>
+        <Route path="./technology/:tech" element={ <TechnologyPage />}/>
 
         <Route path="/*" element={ <Navigate to='/home' /> }/>
   </Routes>
